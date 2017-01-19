970770069673bebb99b776e7642e1c165f4155d0
7059705091 <verma.sahil3232@gmail.com>
2017-01-18 20:20:09 -0500

    Lab js

---
+++ Lab2.js
@@ -0,0 +1,43 @@
+var userChoice = prompt("choose rock, paper or scissors?");
+var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+	computerChoice = "rock";
+} 
+else if (computerChoice <= 0.67) {
+	computerChoice = "paper";
+} 
+else {
+	computerChoice = "scissors";
+}
+
+function compare(choice1, choice2) {
+    if (choice1===choice2) {
+        return "The result is a tie!";
+    }
+    else if (choice1=="rock") {
+        if (choice2==="scissors"){
+            return "rock wins";
+        }
+        else {
+            return "paper wins";
+        }
+    }
+    else if (choice1==="paper") {
+        if (choice2==="rock") {
+            return "paper wins";
+        }
+        else {
+            return "scissors wins";
+        }
+    } else {
+        if (choice2==="rock") {
+            return "rock wins";
+        }
+        else {
+            return "scissors wins";
+        }
+  }
+}
+
+console.log(compare(computerChoice, userChoice));
+console.log("Computer: " + computerChoice + "; User: " + userChoice);
